0d795980cab435bbf39f9e7181adbd956c0efb3a
Lucius Fowler <luciusfow@gmail.com>
2021-01-17 15:14:04 -0500

    commit tree bb8dfe5b354809efa6c4ce009565e4c2ff2e0627

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
 # Add your code here
+require 'pry'
+class Dog 
+    
+    @@all = []
+
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+        # @@all << self
+        # @save = save
+        save
+        # clear_all = @@all.clear
+    end
+    def self.all
+        @@all
+    end
+    def save
+        @@all << self
+    end
+    def self.clear_all
+        @@all.clear #not .clear_all
+        # binding pry
+        # @@all
+    end
+    def self.print_all
+        @@all.each do |doggy|
+            puts doggy.name
+        end
+       
+    end
+end
